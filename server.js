cb1876b6236629264ac533ca4338110747186550
Saismarth Udikeri <saisamarthudikeri@gmail.com>
2024-10-23 18:20:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// API route to verify reCAPTCHA
+app.post("/verify-captcha", (req, res) => {
+    const userResponse = req.body['g-recaptcha-response'];
+    const secretKey = "6Lf32GkqAAAAALyBtB3Rvuoz_CVZIYwzaDZBV8RS";
+    
+    const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${userResponse}`;
+
+    https.get(verificationUrl, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            const result = JSON.parse(data);
+            if (result.success) {
+                res.send({ success: true });
+            } else {
+                res.send({ success: false });
+            }
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        res.send({ success: false });
+    });
+});
 
 // API route to fetch user email
 app.get("/get-email", (req, res) => {
@@ -13,12 +47,10 @@ app.get("/get-email", (req, res) => {
     https.get(user_json_url, (response) => {
         let data = "";
 
-        // Receive chunks of data
         response.on("data", (chunk) => {
             data += chunk;
         });
 
-        // Once all data is received
         response.on("end", () => {
             const jsonData = JSON.parse(data);
             const user_email_id = jsonData.user_email_id;
